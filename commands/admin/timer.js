068d499272473db507176a7fb4d01a99be12f925
Jesse Nguyen <jesse_nguyen9@yahoo.com>
2022-08-27 16:18:32 +1000

    Fix colours and done message for timer command

--- commands/admin/timer.js
+++ commands/admin/timer.js
@@ -7,29 +7,29 @@ module.exports = {
     admin: true,
     usage: `${config.PREFIX}timer <minutes> [title...]`,
     async execute(msg, args) {
-        if(args.length === 0) {
+        if (args.length === 0) {
             msg.reply("Please give an amount of minutes as argument")
             return;
         }
 
         // Ensure minutes is a number
-        let isValid = true;
         let minutes = parseInt(args[0]);
+        let originalMinutes = minutes;
 
-        if(isNaN(minutes) || minutes <= 0) {
+        if (isNaN(minutes) || minutes <= 0) {
             msg.reply("Please give a valid number as argument");
             return;
         }
 
-        if(minutes >= 60) {
+        if (minutes >= 60) {
             msg.reply("No good reason for me to enable timers over 60 minutes");
             return;
         }
 
         // Check if there is a title given
         let title;
-        if(args[1]) {
-            title = args.slice(1,args.length).join(" ");
+        if (args[1]) {
+            title = args.slice(1, args.length).join(" ");
         } else {
             title = "Timer";
         }
@@ -38,16 +38,16 @@ module.exports = {
 
         // Create embed that will act as timer
         const timerEmbed = new Discord.MessageEmbed()
-            .setColor("#0d3b8c")
+            .setColor("#ff0000")
             .setTitle(title)
-            .setDescription(`${minutes} minutes remaining...`)
-        
+            .setDescription(`${minutes} minutes remaining...`);
+
         const oldEmbed = await msg.channel.send(timerEmbed);
         let oldTime = moment.now();
         // Check every second if a minute has passed
         const timerId = setInterval(() => {
             // Check if 1 minute has passed
-            if(moment.now() - oldTime >= 60000) {
+            if (moment.now() - oldTime >= 5000) {
                 oldTime = moment.now()
                 minutes -= 1;
 
@@ -57,11 +57,14 @@ module.exports = {
             }
 
             // Check timer is finished
-            if(minutes === 0) {
+            if (minutes === 0) {
                 clearInterval(timerId);
                 timerActive = false;
-                // oldEmbed.delete();
-                msg.reply("Timer is done!");
+                const editedTimerEmbed = new Discord.MessageEmbed(timerEmbed)
+                    .setDescription("Done!")
+                    .setColor("#4dff00");
+                oldEmbed.edit(editedTimerEmbed);
+                msg.reply(`Timer of ${originalMinutes}min(s) is done!`);
             }
 
         }, 1000);
